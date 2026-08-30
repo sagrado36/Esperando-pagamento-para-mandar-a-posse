d0e415232b1fbac646f647280be4ba886b8c914b
sagrado36 <naru1v4@gmail.com>
2026-08-30 00:48:33 -0300

    index.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "bot-apostas-discord",
+  "version": "1.0.0",
+  "description": "Bot de filas e apostas para Discord",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "engines": {
+    "node": ">=20"
+  },
+  "dependencies": {
+    "discord.js": "^14.25.1"
+  }
+}
